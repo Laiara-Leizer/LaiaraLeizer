1c570073355f0124279380f00be5c6d65344d4f0
Laiara-Leizer <laiaraleizer06@gmail.com>
2024-12-17 19:05:31 -0300

    Atualização Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../../context/LanguageContext';
 import './Home.css';
+import GitHub from '../../img/Github.png';
+import Linkedin from '../../img/Linkedin.png';
+import Send from '../../img/Send.png';
 
 export default function Home() {
   const { language, translations } = useContext(LanguageContext);
@@ -15,18 +18,40 @@ export default function Home() {
         </div>
         <p className="greeting">{t.greeting} {t.about} <p className="cta">{t.cta}</p> </p>
         {/* <p className="about"></p> */}
-        
+
       </div>
       <div className="social-links">
-        <a href="https://github.com/LaiaraLeizer" target="_blank" rel="noopener noreferrer">
-          <img src="/github-icon.png" alt="GitHub" />
+        
+        
+        <a className='fooster_link' href="https://github.com/LaiaraLeizer" target="_blank" rel="noopener noreferrer">
+          <img src={GitHub} alt="GitHub" />
+          <h3>GITHUB ESCOLAR</h3>
         </a>
-        <a href="https://www.linkedin.com/in/laiara-leizer-30b18a318/" target="_blank" rel="noopener noreferrer">
-          <img src="/linkedin-icon.png" alt="LinkedIn" />
+
+        <a className='fooster_link' href="https://github.com/Laiara-Leizer" target="_blank" rel="noopener noreferrer">
+          <img src={GitHub} alt="GitHub_Novo" />
+          <h3>GITHUB PROFICIONAL</h3>
         </a>
-        <a href="mailto:laiaraleizer06@gmail.com">
-          <img src="/email-icon.png" alt="Email" />
+
+
+
+
+
+
+        <a className='fooster_link' href="https://www.linkedin.com/in/laiara-leizer-30b18a318/" target="_blank" rel="noopener noreferrer">
+          <img src={Linkedin} alt="LinkedIn" />
+          <h3>LINKEDIN</h3>
         </a>
+
+
+
+        <a className='fooster_link' href="mailto:laiaraleizer06@gmail.com">
+          <img src={Send} alt="Email" />
+          <h3>EMAIL</h3>
+        </a>
+
+
+
       </div>
     </div>
   );
